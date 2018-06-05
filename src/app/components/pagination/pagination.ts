d783578c0e2834003996dff27a20c9f4e7fe6494
Brad Mallow <brad.mallow@gmail.com>
2018-06-05 01:37:30 -0400

    improve pagination markup

--- src/app/components/pagination/pagination.ts
+++ src/app/components/pagination/pagination.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 
 export class PaginationComponent {
-	@Input() listType  : number;
+	@Input() listType  : string;
 	@Input() page      : number;
 	@Input() pageCount : number;
 }
